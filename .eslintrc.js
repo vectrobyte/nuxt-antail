97ece94cf0991764b4d95c7c7de1391439c7e923
Dilip Bhattarai <vectrobyte@gmail.com>
2019-08-16 20:04:34 +0545

    Add eslintrc config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,58 @@
+const status = (process.env.NODE_ENV === 'production') ? 'error' : 'warn';
+
 module.exports = {
   root: true,
   env: {
+    node: true,
     browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    es6: true,
   },
   extends: [
-    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended',
     'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    '@vue/airbnb',
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  plugins: ['prettier'],
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
-}
+    'nuxt/no-cjs-in-config': 'off',
+    'indent': [
+      status,
+      2
+    ],
+    'quotes': [
+      status,
+      'single'
+    ],
+    'semi': [
+      status,
+      'always'
+    ],
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    "camelcase": [
+      0, {
+        "properties": "never"
+      }
+    ],
+    "no-underscore-dangle": [
+      0, {
+        "properties": "never"
+      }
+    ],
+    'no-console': status,
+    'no-debugger': status,
+    'max-len': 'off',
+    'linebreak-style': ["error", "unix"],
+    'import/prefer-default-export': 'off',
+    'vue/html-closing-bracket-newline': [status, {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};

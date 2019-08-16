4ef55a3bc7b401eb6a506caa7cfc46dfeb6cc02f
Dilip Bhattarai <vectrobyte@gmail.com>
2019-08-17 00:12:23 +0545

    Define a base layout

---
+++ layouts/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer
+    class="py-4 w-full"
+    :class="[bgColor]">
+    <Container>
+      <FlexBox
+        align-items="center"
+        justify="between">
+        Footer Area
+      </FlexBox>
+    </Container>
+  </footer>
+</template>
+
+<script>
+import Container from '../../components/base/Container';
+import FlexBox from '../../components/base/FlexBox';
+
+export default {
+  name: 'Footer',
+  components: { FlexBox, Container },
+  props: {
+    color: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    bgColor() {
+      const { color } = this;
+      return color ? `bg-${color}` : '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .icons-group img {
+    margin: 0 0.5rem;
+  }
+</style>
---
+++ layouts/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="py-4">
+    <Container>
+      <FlexBox justify="between">
+        <NuxtLink to="/">
+          Icon here
+        </NuxtLink>
+
+        <FlexBox
+          class="buttons-group">
+          <Button
+            color="primary"
+            size="large"
+            class="mr-2"
+            @click="$emit('login')">
+            Login
+          </Button>
+
+          <AButton
+            type="default"
+            size="large"
+            @click="$emit('register')">
+            Sign up
+          </AButton>
+        </FlexBox>
+      </FlexBox>
+    </Container>
+  </nav>
+</template>
+
+<script>
+import Button from '../../components/form/Button';
+import FlexBox from '../../components/base/FlexBox';
+import Container from '../../components/base/Container';
+
+export default {
+  layout: 'default',
+  name: 'Header',
+  components: {
+    Container,
+    FlexBox,
+    Button,
+  },
+  props: {
+    isLogged: {
+      type: Boolean,
+    },
+    user: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+  .active-icon {
+    top: 10px;
+    right: 0;
+    line-height: 0;
+    color: #00e635;
+  }
+</style>
